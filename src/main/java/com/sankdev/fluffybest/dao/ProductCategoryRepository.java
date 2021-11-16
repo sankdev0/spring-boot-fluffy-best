64b73fc97c852e3d00ee1d134418b8eb1d3309e5
san k <sankdev0@gmail.com>
2021-11-17 00:02:22 +0500

    Add Cross Origin Support to ProductRepository
    and ProductCategoryRepository

--- src/main/java/com/sankdev/fluffybest/dao/ProductCategoryRepository.java
+++ src/main/java/com/sankdev/fluffybest/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.sankdev.fluffybest.dao;
 import com.sankdev.fluffybest.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 // collectionResourceRel means the nate of the JSON entry
 // path means reference for the path to api ("/product-category")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
