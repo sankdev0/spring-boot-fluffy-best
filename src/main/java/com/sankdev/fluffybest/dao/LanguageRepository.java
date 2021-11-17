b26e564ecafa9f934741482acfc671e38390fb3d
san k <sankdev0@gmail.com>
2021-11-18 00:27:34 +0500

    Add Cross Origin Support to LanguageRepository
    for origin http://localhost:4200.
    *Origin is protocol+hostname+port.

--- src/main/java/com/sankdev/fluffybest/dao/LanguageRepository.java
+++ src/main/java/com/sankdev/fluffybest/dao/LanguageRepository.java
@@ -2,7 +2,9 @@ package com.sankdev.fluffybest.dao;
 
 import com.sankdev.fluffybest.entity.Language;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface LanguageRepository extends JpaRepository<Language, Long> {
 
 }
