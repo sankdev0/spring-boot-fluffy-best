2eb8a6bc154b4a7d94dc38cbba4e831ae1b6b128
san k <sankdev0@gmail.com>
2021-11-28 23:22:51 +0500

    Add CountryTranslationRepository

---
+++ src/main/java/com/sankdev/fluffybest/dao/location/CountryTranslationRepository.java
@@ -0,0 +1,10 @@
+package com.sankdev.fluffybest.dao.location;
+
+import com.sankdev.fluffybest.entity.location.CountryTranslation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "countryTranslation", path = "country-translation")
+public interface CountryTranslationRepository extends JpaRepository<CountryTranslation, Long> {
+
+}
