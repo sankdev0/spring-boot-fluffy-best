47e10036ee896f2a811d5ce85c8792fa405c78dc
san k <sankdev0@gmail.com>
2022-01-12 17:14:53 +0500

    Refactor: Remove BaseClass and NamedEntity
    Entity Super classes BaseClass holding id field and NamedEntity holding name field are removed.

--- src/main/java/com/sankdev/fluffybest/entity/Language.java
+++ src/main/java/com/sankdev/fluffybest/entity/Language.java
@@ -2,13 +2,23 @@ package com.sankdev.fluffybest.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Table(name = "language")
 @Data
-public class Language extends NamedEntity {
+public class Language {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name")
+  private String name;
 
   @Column(name = "alpha_two_code")
   private String alphaTwoCode;
--- src/main/java/com/sankdev/fluffybest/entity/Product.java
+++ src/main/java/com/sankdev/fluffybest/entity/Product.java
@@ -4,6 +4,9 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -14,7 +17,14 @@ import org.hibernate.annotations.UpdateTimestamp;
 @Entity
 @Table(name = "product")
 @Data
-public class Product extends NamedEntity {
+public class Product {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name")
+  private String name;
 
   @Column(name = "sku")
   private String sku;
