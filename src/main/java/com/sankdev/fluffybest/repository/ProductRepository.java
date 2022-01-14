c1ac93b8d417ef079c885d1d77d61ad62c808c72
san k <sankdev0@gmail.com>
2022-01-14 16:04:47 +0500

    Fix findByCategoryId bug - wrong param type

--- src/main/java/com/sankdev/fluffybest/repository/ProductRepository.java
+++ src/main/java/com/sankdev/fluffybest/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.sankdev.fluffybest.repository;
 
 import com.sankdev.fluffybest.entity.Product;
+import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200") // accept calls from web browser scripts from this origin
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, UUID> {
 
   // It is a query method. Behind the scenes Spring Data will execute a query similar to this:
   // SELECT * FROM product WHERE category_id=?
